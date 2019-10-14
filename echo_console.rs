332dec7cc814fa666d923c692fe14c04d3aac320
rp28ap <ravindra@sparktg.com>
2019-10-14 22:04:16 +0530

    testing merge conflict

--- echo_console.rs
+++ echo_console.rs
@@ -36,6 +36,7 @@ fn main() {
             let stdin = tokio_stdin_stdout::stdin(0);
             let stdout = tokio_stdin_stdout::stdout(0); // .make_sendable();
 
+            // This comment is added in the branch.
             let stdin = FramedRead::new(stdin, LinesCodec::new());
             let stdout = FramedWrite::new(stdout, LinesCodec::new());
          
